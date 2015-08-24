970e436df997db37edcf8a0f55a2836c91229fc3
Pablo Galindo Salgado <pablogsal@gmail.com>
2015-08-24 18:49:21 +0200

    Eliminate the output file opening in main

--- HADES.py
+++ HADES.py
@@ -150,8 +150,7 @@ def main():
 
     # Open the output_file
     logger.info('Opening the output file ' + par.result_file + '.')
-    f = open(par.result_file, "w")
-
+    
     # Pass the magnetic field to cgs and correct the density and eps with the
     # tracer values
     data.correction(par.external_density)
